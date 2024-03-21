afed982f9b6e0957e7a34bbabda3fa7dd19c3545
Delibes Bechir BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2024-03-21 10:05:19 +0100

    tests: scenario where removing default vhosts

---
+++ molecule/vhosts_without_default/tests/test_default.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def get_ip_address(host):
+    return host.interface("eth0").addresses[0]
+
+
+def test_apache_mod_status(host):
+    request = requests.head(
+        url=f"https://{get_ip_address(host)}/server-status", verify=False
+    )
+
+    assert request.status_code == 403 or request.status_code == 200
+
+
+def test_apache_vhost_http_auth_fpm(host):
+    request = requests.get(
+        url=f"http://{get_ip_address(host)}/var.php",
+        headers={"Host": "molecule.example.com"},
+        auth=HTTPBasicAuth("molecule-user", "molecule-pass"),
+        verify=False,
+    )
+
+    assert 200 == request.status_code
+
+
+def test_apache_letsencrypt_challenge(host):
+    request = requests.get(
+        url=f"https://{get_ip_address(host)}/.well-known/acme-challenge/challenge.html",
+        headers={"Host": "certbot.retail.example.com"},
+        auth=HTTPBasicAuth("molecule-fpm-user", "molecule-fpm-pass"),
+        verify=False,
+    )
+
+    assert "This is a letsencrypt challenge" in request.text
+    assert 200 == request.status_code
